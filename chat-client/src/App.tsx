d7bcdd8041ff82bcdf7b8e46a885f60c4e1e02da
abhinav3254 <abhinav3254@gmail.com>
2024-02-20 13:26:05 +0530

    first commit

---
+++ chat-client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SideNav from './side-nav/SideNav';
+
+function App() {
+  return (
+    <>
+      <SideNav />
+    </>
+  );
+}
+
+export default App;
