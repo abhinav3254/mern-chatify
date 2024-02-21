22ea3f0e04f049288e5f52dd4fd556c656c92075
abhinav3254 <abhinav3254@gmail.com>
2024-02-22 00:19:06 +0530

    chat json done

--- chat-client/src/Chat/Chat.tsx
+++ chat-client/src/Chat/Chat.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Chat.scss'
 import menuImage from '../images/svg/menu.svg';
-import sendImage from '../images/svg/send.svg'
+import sendImage from '../images/svg/send.svg';
+import personChatting from '../json/person-chatting.json'
 
 
 function Chat() {
@@ -10,15 +11,20 @@ function Chat() {
 
             {/* chat nav bar */}
             <div className='chatNav'>
-                <img className='dp' src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
-                <h2>Ansh Jha</h2>
+                <img className='dp' src={personChatting.userImage} alt="" />
+                <h2>{personChatting.personName}</h2>
                 <img className='menu' src={menuImage} alt="" />
             </div>
 
             <div className='chat-body-area'>
-                <div className="chat-to"></div>
-                <div className="chat-from"></div>
+                {personChatting.conversation.map((data, index) => (
+                    <div key={index} className={data.name === 'user1' ? "chat-from" : "chat-to"}>
+                        <p>{data.message}</p>
+                        <p className="time">{data.time}</p>
+                    </div>
+                ))}
             </div>
+
 
             <div className="typing-area">
                 <input type="text" placeholder='Message' />
