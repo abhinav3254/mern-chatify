dd4255954269128d599f8d898da34fcd706c7617
abhinav3254 <abhinav3254@gmail.com>
2024-02-22 23:57:10 +0530

    hacker-rank solution

---
+++ node.js-certify/question-1/index.js
@@ -0,0 +1,18 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.send('<p>HTML Data</p>');
+// });
+
+// module.exports = router;
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('<p>HTML Data</p>');
+});
+
+module.exports = router;
