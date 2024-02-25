b97b6286b7b4f26fa6e04471dfb343ee068fe13d
abhinav3254 <abhinav3254@gmail.com>
2024-02-25 16:17:19 +0530

    changes

---
+++ chat-client/src/components/ChatNav/ChatNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ChatNav.scss'
+
+const ChatNav = () => {
+    return (
+        <div className='ChatNav'>
+            <img src="https://images.unsplash.com/photo-1441123694162-e54a981ceba5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cnVzc2luYSUyMGdpcmx8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Lorem ipsum</p>
+        </div>
+    )
+}
+
+export default ChatNav
