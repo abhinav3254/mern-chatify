828c08237977871a7d36615c47181160835949ea
abhinav3254 <abhinav3254@gmail.com>
2024-04-07 19:40:09 +0530

    done

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -9,6 +9,7 @@ function Chat() {
     const [onlinePeople, setOnlinePeople] = useState({});
     const [selectedUserId, setSelectedUserId] = useState(null);
     const { username, id } = useContext(UserContext);
+    const [newMessageText, setNewMessageText] = useState('');
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000');
@@ -29,6 +30,16 @@ function Chat() {
         if ('ononline' in messageData) {
             showOnlinePeople(messageData.ononline);
         }
+    }
+
+    function sendMessage(e) {
+        e.preventDefault();
+        ws.send(JSON.stringify({
+            message: {
+                recipient: selectedUserId,
+                text: newMessageText
+            }
+        }));
     }
 
     // deleting our user from the JSON Object
@@ -55,15 +66,29 @@ function Chat() {
                 ))}
             </div>
             <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-                <div className="flex-grow">messages</div>
-                <div className="flex gap-2">
-                    <input type="text" placeholder='Message' className="bg-white border p-2 flex-grow rounded-sm" />
-                    <button className="bg-blue-500 p-2 text-white rounded-sm">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                        </svg>
-                    </button>
+                <div className="flex-grow">
+                    {!selectedUserId && (
+                        <div className="flex items-center justify-center h-full">
+                            <div className="text-gray-300">&larr; selected a person to chat</div>
+                        </div>
+                    )}
                 </div>
+
+                {/* double !! means it will convert the value into boolean */}
+                {!!selectedUserId && (
+                    <form className="flex gap-2" onSubmit={sendMessage}>
+                        <input type="text" placeholder='Message'
+                            value={newMessageText}
+                            onChange={(e) => setNewMessageText(e.target.value)}
+                            className="bg-white border p-2 flex-grow rounded-sm"
+                        />
+                        <button type='submit' className="bg-blue-500 p-2 text-white rounded-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                        </button>
+                    </form>
+                )}
             </div>
         </div>
     )
